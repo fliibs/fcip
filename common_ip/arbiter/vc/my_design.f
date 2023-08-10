cb7c44b0bf897e48b2f3c0cd8ba60f56b9034027
StevenHuang5v <htcannotregister@163.com>
2023-08-10 17:52:29 +0800

    update common ip

---
+++ common_ip/arbiter/vc/my_design.f
@@ -0,0 +1,18 @@
+// /home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/tb/arb_fp_tb.sv
+// /home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/tb/arb_rr_tb.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/tb/arb_matrix_tb.sv
+
+
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/arb_fp.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/arb_rr.sv
+
+// age_matrix grant
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/age_matrix.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/arb_matrix.sv 
+
+// age matrix vrp 
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/real_mux_onehot.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/arb_vrp_matrix.sv 
+
+// mode select 
+/home/liuyunqi/huangtao/common_ip/CommonIP/arbiter/rtl/arb_vrp.sv
---
+++ common_ip/reg_slice/vc/my_design.f
@@ -0,0 +1,5 @@
+
+/home/liuyunqi/huangtao/common_ip/CommonIP/reg_slice/rtl/reg_slice_forward.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/reg_slice/rtl/reg_slice_backward.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/reg_slice/rtl/reg_slice_full.sv
+/home/liuyunqi/huangtao/common_ip/CommonIP/reg_slice/rtl/reg_slice.sv
