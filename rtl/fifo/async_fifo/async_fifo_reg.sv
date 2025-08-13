3f0cbbe18ba7ef2d6901966c815893b38e00df7c
jiaoyadi <iejiaoyadi@163.com>
2025-08-13 17:40:22 +0800

    update afifo

--- rtl/fifo/async_fifo/async_fifo_reg.sv
+++ rtl/fifo/async_fifo/async_fifo_reg.sv
@@ -1,6 +1,8 @@
 module async_fifo_reg #(
     parameter  integer unsigned FIFO_DEPTH = 16,
-    parameter  integer unsigned FIFO_WIDTH = 16
+    parameter  integer unsigned DATA_WIDTH = 16,
+    parameter integer unsigned  FULL_ZERO  = 0,
+    parameter integer unsigned  SYNC_STAGE = 2
 )(
     input  logic                    wclk,
     input  logic                    rclk,
@@ -8,55 +10,73 @@ module async_fifo_reg #(
     input  logic                    rrst_n,
 
     //power down
-    input  logic                    stall,
-    input  logic                    clear,
+    input  logic                    read_stall,
+    input  logic                    write_stall,
+    input  logic                    read_clear,
+    input  logic                    write_clear,
+
+    output logic                    read_full_zero,
+    output logic                    write_full_zero,
+
     output logic                    idle,
 
     //write req
     input  logic                    write_req_vld,
-    input  logic [FIFO_WIDTH-1:0]   write_req_pld,
+    input  logic [DATA_WIDTH-1:0]   write_req_pld,
     output logic                    write_req_rdy,
 
     //read response
     output logic                    read_resp_vld,
-    output logic [FIFO_WIDTH-1:0]   read_resp_pld,
+    output logic [DATA_WIDTH-1:0]   read_resp_pld,
     input  logic                    read_resp_rdy
 );
 
 logic [FIFO_DEPTH-1:0]   wptr_async;
 logic [FIFO_DEPTH-1:0]   rptr_async;
 logic [FIFO_DEPTH-1:0]   rptr_sync;
-logic [FIFO_WIDTH-1:0]   pld_sync;
+logic [DATA_WIDTH:0]     pld_sync;
 
-afifo_write_domain #(
+afifo_slv #(
     .FIFO_DEPTH (FIFO_DEPTH),
-    .FIFO_WIDTH (FIFO_WIDTH)
-) u_afifo_write_domain(
+    .DATA_WIDTH (DATA_WIDTH),
+    .FULL_ZERO  (FULL_ZERO),
+    .SYNC_STAGE (SYNC_STAGE)
+) u_afifo_slv(
     .wclk           (wclk),
     .wrst_n         (wrst_n),
-    .stall          (stall),
-    .clear          (clear),
+
+    .write_stall    (write_stall),
+    .write_clear    (write_clear),
+    .write_full_zero(write_full_zero),
+
     .write_req_vld  (write_req_vld),
     .write_req_pld  (write_req_pld),
     .write_req_rdy  (write_req_rdy),
+
     .wptr_async     (wptr_async),
     .rptr_async     (rptr_async),
     .rptr_sync      (rptr_sync),
     .pld_sync       (pld_sync)
 );
 
-afifo_read_domain #(
+afifo_mst #(
     .FIFO_DEPTH (FIFO_DEPTH),
-    .FIFO_WIDTH (FIFO_WIDTH)
-) u_afifo_read_domain(
+    .DATA_WIDTH (DATA_WIDTH),
+    .FULL_ZERO  (FULL_ZERO),
+    .SYNC_STAGE (SYNC_STAGE)
+) u_afifo_mst(
     .rclk           (rclk),
     .rrst_n         (rrst_n),
-    .stall          (stall),
-    .clear          (clear),
-    .idle           (idle),
+
+    .read_stall     (read_stall),
+    .read_clear     (read_clear),
+    .read_full_zero (read_full_zero),
+    .read_idle      (idle),
+
     .read_resp_vld  (read_resp_vld),
     .read_resp_pld  (read_resp_pld),
     .read_resp_rdy  (read_resp_rdy),
+
     .wptr_async     (wptr_async),
     .rptr_async     (rptr_async),
     .rptr_sync      (rptr_sync),
