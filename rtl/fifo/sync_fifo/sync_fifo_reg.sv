46e8d7fbdc49490b88cc6bca7f6198dfd84ce7d7
jiaoyadi <iejiaoyadi@163.com>
2025-08-14 16:50:43 +0800

    update sync_fifo

--- rtl/fifo/sync_fifo/sync_fifo_reg.sv
+++ rtl/fifo/sync_fifo/sync_fifo_reg.sv
@@ -1,7 +1,9 @@
 module sync_fifo_reg #(
     parameter  integer unsigned FIFO_DEPTH = 16,
     parameter  integer unsigned FIFO_WIDTH = 16,
-    parameter  integer unsigned THRESHOLD  = 8,
+    parameter  integer unsigned ALMOST_FULL_THRESHOLD  = 12,
+    parameter  integer unsigned ALMOST_EMPTY_THRESHOLD = 4 ,
+    parameter  integer unsigned FORWARD_EN = 1 ,
     localparam int unsigned CNT_WIDTH = $clog2(FIFO_DEPTH)
 )(
     input  logic                    clk,
@@ -22,34 +24,50 @@ module sync_fifo_reg #(
     output logic [FIFO_WIDTH-1:0]   read_resp_pld,
     input  logic                    read_resp_rdy,
 
-    output logic                    custom_threshold_en,
+    output logic                    almost_full,
+    output logic                    almost_empty,
     output logic                    empty,
     output logic                    full
 );
 
-logic [CNT_WIDTH:0]             wr_ptr;
-logic [CNT_WIDTH:0]             rd_ptr;
-logic [CNT_WIDTH-1:0]           wr_ptr_true;
-logic [CNT_WIDTH-1:0]           rd_ptr_true;
-logic                           wr_ptr_msb;
-logic                           rd_ptr_msb;
+logic [CNT_WIDTH-1:0]           wr_ptr;
+logic [CNT_WIDTH-1:0]           rd_ptr;
+//logic [CNT_WIDTH-1:0]           wr_ptr_true;
+//logic [CNT_WIDTH-1:0]           rd_ptr_true;
+//logic                           wr_ptr_msb;
+//logic                           rd_ptr_msb;
 logic [FIFO_WIDTH-1:0]          array_data[FIFO_DEPTH-1:0];
-logic [CNT_WIDTH-1:0]           fifo_used;
+logic [CNT_WIDTH:0]             ptr_cnt;
+logic                           rinc;
+logic                           winc;
 
 /*========================================*/
-/*              read control              */
+/*            read/write control          */
 /*========================================*/
 
-assign read_resp_pld    = array_data[rd_ptr_true];
-assign read_resp_vld    = ~empty;
-assign rinc             = (~empty) && read_resp_rdy;
+generate 
+    if(FORWARD_EN) begin
 
-/*========================================*/
-/*             write control              */
-/*========================================*/
+        logic forward_enable;
 
-assign write_req_rdy    = ~full;
-assign winc             = write_req_vld && write_req_rdy;
+        assign forward_enable   = empty && read_resp_rdy;
+        assign read_resp_pld    = forward_enable ? write_req_pld : array_data[rd_ptr];
+        assign read_resp_vld    = forward_enable ? write_req_vld : ~empty;
+
+        assign rinc             = (~empty) && read_resp_rdy;
+        assign write_req_rdy    = ~full;
+        assign winc             = write_req_vld && write_req_rdy && ~forward_enable;
+
+    end else begin
+        assign read_resp_pld    = array_data[rd_ptr];
+        assign read_resp_vld    = ~empty;
+
+        assign rinc             = (~empty) && read_resp_rdy;
+
+        assign write_req_rdy    = ~full;
+        assign winc             = write_req_vld && write_req_rdy;
+    end
+endgenerate
 
 /*========================================*/
 /*           read & write counter         */
@@ -69,36 +87,68 @@ always_ff @( posedge clk or negedge rst_n ) begin
         wr_ptr <= wr_ptr + 1'b1;
 end
 
-assign fifo_used            = (wr_ptr >= rd_ptr) ? (wr_ptr - rd_ptr): (wr_ptr + FIFO_DEPTH - rd_ptr);
-assign custom_threshold_en  = (fifo_used == THRESHOLD);
+always_ff @( posedge clk or negedge rst_n ) begin
+    if(~rst_n)
+        ptr_cnt <= 'b0;
+    else if(winc && rinc)
+        ptr_cnt <= ptr_cnt;
+    else if(winc)
+        ptr_cnt <= ptr_cnt + 1'b1;
+    else if(rinc)
+        ptr_cnt <= ptr_cnt - 1'b1;
+end
+
+/*========================================*/
+/*              Almost signals            */
+/*========================================*/
+
+always_ff @( posedge clk or negedge rst_n ) begin
+    if(~rst_n)
+        almost_full <= 'b0;
+    else if(ptr_cnt >= ALMOST_FULL_THRESHOLD)
+        almost_full <= 1'b1;
+    else 
+        almost_full <= 1'b0;
+end
+
+always_ff @( posedge clk or negedge rst_n ) begin
+    if(~rst_n)
+        almost_empty <= 'b0;
+    else if(ptr_cnt <= ALMOST_EMPTY_THRESHOLD)
+        almost_empty <= 1'b1;
+    else 
+        almost_empty <= 1'b0;
+end
+
+//assign fifo_used            = (wr_ptr >= rd_ptr) ? (wr_ptr - rd_ptr): (wr_ptr + FIFO_DEPTH - rd_ptr);
 
 /*========================================*/
 /*               pointer check            */
 /*========================================*/
 
-assign {wr_ptr_msb,wr_ptr_true} = wr_ptr;
-assign {rd_ptr_msb,rd_ptr_true} = rd_ptr;
+//assign {wr_ptr_msb,wr_ptr_true} = wr_ptr;
+//assign {rd_ptr_msb,rd_ptr_true} = rd_ptr;
 
-//always_ff @( posedge clk or negedge rst_n ) begin
-//    if(~rst_n)
-//        full <= 'b0;
-//    else if((rd_ptr_true == wr_ptr_true) && (wr_ptr_msb != rd_ptr_msb))
-//        full <= 1'b1;
-//    else 
-//        full <= 1'b0;
-//end
+always_ff @( posedge clk or negedge rst_n ) begin
+    if(~rst_n)
+        full <= 'b0;
+    else if( (ptr_cnt == (FIFO_DEPTH-1)) && winc && ~rinc)
+        full <= 1'b1;
+    else if( ptr_cnt <= (FIFO_DEPTH-1) )
+        full <= 1'b0;
+end
 
-//always_ff @( posedge clk or negedge rst_n ) begin
-//    if(~rst_n)
-//        empty <= 'b0;
-//    else if(rd_ptr == wr_ptr)
-//        empty <= 1'b1;
-//    else 
-//        empty <= 1'b0;
-//end
+always_ff @( posedge clk or negedge rst_n ) begin
+    if(~rst_n)
+        empty <= 'b1;
+    else if( (ptr_cnt == 1) && rinc && ~winc )
+        empty <= 1'b1;
+    else if( ptr_cnt >= 1 )
+        empty <= 1'b0;
+end
 
-assign full     = (rd_ptr_true == wr_ptr_true) && (wr_ptr_msb != rd_ptr_msb);
-assign empty    = (rd_ptr == wr_ptr);
+//assign full     = (rd_ptr_true == wr_ptr_true) && (wr_ptr_msb != rd_ptr_msb);
+//assign empty    = (rd_ptr == wr_ptr);
 
 /*========================================*/
 /*                Reg entry               */
@@ -109,7 +159,7 @@ generate
         always_ff @( posedge clk or negedge rst_n ) begin : DATA_ARRAY
             if(~rst_n)
                 array_data[i] <= 'b0;
-            else if( winc && (wr_ptr_true==i))
+            else if( winc && (wr_ptr==i))
                 array_data[i] <= write_req_pld;
         end
     end
