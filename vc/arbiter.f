93ad280fa78e828d7eec9c19eaff99845e5be8a5
ymy98 <3526365076@qq.com>
2026-01-21 10:10:10 +0800

    add missing ips

--- vc/arbiter.f
+++ vc/arbiter.f
@@ -5,3 +5,8 @@ $FCIP_DIR/ip/arbiter/fcip_grant_gen_fp.sv
 $FCIP_DIR/ip/arbiter/fcip_grant_gen_rr.sv
 $FCIP_DIR/ip/arbiter/fcip_mtx_gen_age.sv
 $FCIP_DIR/ip/arbiter/fcip_mtx_gen_plru_tree.sv
+$FCIP_DIR/ip/arbiter/fcip_tree_plru_comb.sv
+$FCIP_DIR/ip/arbiter/fcip_arb_vr_matrix.sv
+$FCIP_DIR/ip/arbiter/fcip_age_matrix_list.sv
+$FCIP_DIR/ip/arbiter/fcip_age_matrix.sv
+$FCIP_DIR/ip/arbiter/fcip_arb_vrp_matrix.sv
--- vc/regslice.f
+++ vc/regslice.f
@@ -1,1 +1,2 @@
 $FCIP_DIR/ip/regslice/fcip_reg_slice.sv
+$FCIP_DIR/ip/regslice/fcip_reg_slice_full_set.sv
