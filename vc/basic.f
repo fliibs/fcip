a68c4544579c9149fb2479aa0c5010a2908654b8
band419 <band419gbx@gmail.com>
2026-03-11 19:52:58 +0800

    add list leading zero

--- vc/basic.f
+++ vc/basic.f
@@ -7,6 +7,7 @@ $FCIP_DIR/ip/basic/fcip_lead_one_rev.sv
 $FCIP_DIR/ip/basic/fcip_lfsr4.sv
 $FCIP_DIR/ip/basic/fcip_list_lead_one.sv
 $FCIP_DIR/ip/basic/fcip_list_lead_one_rev.sv
+$FCIP_DIR/ip/basic/fcip_list_lead_zero.sv
 $FCIP_DIR/ip/basic/fcip_onehot2bin.sv
 $FCIP_DIR/ip/basic/fcip_real_mux_onehot.sv
 $FCIP_DIR/ip/basic/fcip_rob_id_dec.sv
