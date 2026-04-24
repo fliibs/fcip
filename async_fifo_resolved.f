98228b88075de50f018d76f8d6b550c7076ea47b
luhz <hongzlu@163.com>
2026-04-24 10:18:13 +0800

    fix: resolved FCIP_DIR paths for remote compile

---
+++ async_fifo_resolved.f
@@ -0,0 +1,16 @@
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo_mst.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo_slv.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_apb_afifo.sv
+
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_axi_afifo/fcip_axi_afifo_mst.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_axi_afifo/fcip_axi_afifo_slv.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_axi_afifo/fcip_axi_afifo.sv
+
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_req_rsp_afifo_mst.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_req_rsp_afifo_slv.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_req_rsp_afifo.sv
+
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo_doub.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo_mst_doub.sv
+/data/luhongzhao/byd/a5_lwnoc/sync/lwnoc_network_verif/subs/fcip/ip/async_fifo/fcip_afifo_slv_doub.sv
