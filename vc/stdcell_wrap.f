da896847039211ab66146e46eb5f0752d2beb7be
lgzhu <1066021972@qq.com>
2026-05-06 11:06:48 +0800

    feat(fusa): add fcip_fusa_pulse_gen with configurable pulse width

--- vc/stdcell_wrap.f
+++ vc/stdcell_wrap.f
@@ -4,3 +4,4 @@ $FCIP_DIR/ip/stdcell_wrap/fcip_data_pipe.sv
 $FCIP_DIR/ip/stdcell_wrap/fcip_sync_arst.sv
 $FCIP_DIR/ip/stdcell_wrap/fcip_sync_aset.sv
 $FCIP_DIR/ip/stdcell_wrap/fcip_sync_cell.sv
+$FCIP_DIR/ip/stdcell_wrap/fcip_fusa_pulse_gen.sv
